8b81cea616e91ba6582d73d5321acda4a6deeea2
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-29 17:24:34 +0530

    config next to use custom fonts

--- next.config.js
+++ next.config.js
@@ -3,10 +3,11 @@ const webpack = require('webpack');
 const withImages = require('next-images');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
+const withFonts = require('next-fonts');
 const withPlugins = require('next-compose-plugins');
 
-module.exports = withPlugins([withCSS, withSass, withImages], {
-    webpack(config) {
+module.exports = withPlugins([withCSS, withSass, withImages, withFonts], {
+    webpack(config, options) {
         return config;
     },
 });
