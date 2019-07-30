488f33cfcd99ed8ee606df230adda9293b26376b
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-07-30 20:25:04 +0530

    add styling to experience-tab contents

--- components/Experience/DetailsHolder.js
+++ components/Experience/DetailsHolder.js
@@ -5,15 +5,83 @@ import { Col, Tab } from 'react-bootstrap';
 const DetailsWrapper = styled(Col)`
     padding-top: 7px;
     padding-left: 30px;
+
+    h2 {
+        font-size: 20px;
+        color: ${props => {
+            return props.theme.blueWhite;
+        }};
+        font-weight: ${props => {
+            return props.theme.semibold;
+        }};
+
+        .company__at {
+            color: ${props => {
+                return props.theme.whiteYellow;
+            }};
+
+            .company__name {
+                color: ${props => {
+                    return props.theme.whiteYellow;
+                }};
+                position: relative;
+                display: inline-block;
+
+                &:hover {
+                    color: ${props => {
+                        return props.theme.whiteYellow;
+                    }};
+                }
+            }
+        }
+    }
+
+    h5 {
+        font-size: 14px;
+        margin-bottom: 30px;
+        font-weight: normal;
+        letter-spacing: 0.5px;
+        font-family: ${props => {
+            return props.theme.sfmono;
+        }};
+    }
+
+    ul {
+        font-size: 16px;
+
+        li {
+            position: relative;
+            padding-left: 30px;
+            margin-bottom: 10px;
+            color: ${props => {
+                return props.theme.lightBlue;
+            }};
+
+            &::before {
+                content: '▹';
+                position: absolute;
+                left: 0px;
+                color: ${props => {
+                    return props.theme.whiteYellow;
+                }};
+                line-height: 20px;
+            }
+
+            .list__item--Link {
+                color: ${props => {
+                    return props.theme.whiteYellow;
+                }};
+            }
+        }
+    }
 `;
 
 const DetailsHolder = () => {
     return (
         <DetailsWrapper sm={9}>
             <Tab.Content>
-                {/* show active classNames */}
                 <Tab.Pane eventKey="first">
-                    <h2 className="exp__title">
+                    <h2>
                         Full Stack Developer
                         <span className="company__at">
                             &nbsp;@&nbsp;
@@ -22,41 +90,37 @@ const DetailsHolder = () => {
                             </a>
                         </span>
                     </h2>
-                    <h5 className="exp__timeline">Mar 2019 - Present</h5>
-                    <ul className="exp__list">
-                        <li className="exp__list--item">Develop and maintain code for in-house and client websites</li>
-                        <li className="exp__list--item">
+                    <h5>Feb 2019 - Present</h5>
+                    <ul>
+                        <li>Develop and maintain code for in-house and client websites</li>
+                        <li>
                             Writes modern, performant, maintainable code for a diverse array of clients and internal
                             projects
                         </li>
                     </ul>
                 </Tab.Pane>
                 <Tab.Pane eventKey="second">
-                    <h2 className="exp__title">Front-End Developer</h2>
-                    <h5 className="exp__timeline">Jan 2018 - Feb 2019</h5>
-                    <ul className="exp__list">
-                        <li className="exp__list--item">
-                            Developed static websites for clients and internal projects.
-                        </li>
-                        <li className="exp__list--item">Occasionally contributed to Open-Source community.</li>
-                        <li className="exp__list--item">
-                            Wrote modern, performant, maintainable code for internal projects
-                        </li>
+                    <h2>Front-End Developer</h2>
+                    <h5>Jan 2018 - Feb 2019</h5>
+                    <ul>
+                        <li>Developed static websites for clients and internal projects.</li>
+                        <li>Occasionally contributed to Open-Source community.</li>
+                        <li>Wrote modern, performant, maintainable code for internal projects</li>
                     </ul>
                 </Tab.Pane>
                 <Tab.Pane eventKey="third">
-                    <h2 className="exp__title">Designer</h2>
-                    <h5 className="exp__timeline">Aug 2018 - Present</h5>
-                    <ul className="exp__list">
-                        <li className="exp__list--item">Designed various Logos, Posters, & UI for the clients</li>
-                        <li className="exp__list--item">
+                    <h2>Designer</h2>
+                    <h5>Aug 2018 - Present</h5>
+                    <ul>
+                        <li>Designed various Logos, Posters, & UI for the clients</li>
+                        <li>
                             Includes works for in-house and clients for various occations. Some of the works were
                             featured by the community.
                         </li>
-                        <li className="exp__list--item">
+                        <li>
                             Check out some designs at{' '}
                             <a
-                                className="exp__list--itemLink"
+                                className="list__item--Link"
                                 title="View"
                                 href="https://www.behance.net/abhijithvijayan"
                                 data-toggle="tooltip"
@@ -70,7 +134,7 @@ const DetailsHolder = () => {
                     </ul>
                 </Tab.Pane>
                 <Tab.Pane eventKey="forth">
-                    <h2 className="exp__title">
+                    <h2>
                         Tech Lead
                         <span className="company__at">
                             &nbsp;@&nbsp;
@@ -87,17 +151,15 @@ const DetailsHolder = () => {
                             </a>
                         </span>
                     </h2>
-                    <h5 className="exp__timeline">Aug 2016 - Present</h5>
-                    <ul className="exp__list">
-                        <li className="exp__list--item">Lead of the Technical Activities conducted by TRACE CEA</li>
-                        <li className="exp__list--item">
-                            Developed and Contributed to the projects hosted for the student community
-                        </li>
-                        <li className="exp__list--item">
+                    <h5>Aug 2016 - Present</h5>
+                    <ul>
+                        <li>Lead of the Technical Activities conducted by TRACE CEA</li>
+                        <li>Developed and Contributed to the projects hosted for the student community</li>
+                        <li>
                             Collaborated with a small team of designers and developers to spearhead new designs and
-                            projects for student-led organisation at
+                            projects for student-led organisation at{' '}
                             <a
-                                className="exp__list--itemLink"
+                                className="list__item--Link"
                                 title="Visit"
                                 href="https://github.com/ceadoor"
                                 data-toggle="tooltip"
