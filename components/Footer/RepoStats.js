1498a161fa2a1444fa544a8d94e7db68b2184bb4
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-08-08 01:27:59 +0530

    add axios to fetch repo details from github api

--- components/Footer/RepoStats.js
+++ components/Footer/RepoStats.js
@@ -1,5 +1,7 @@
-import React from 'react';
+/* eslint-disable camelcase */
+import React, { Component } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const RepoStatsWrapper = styled.div`
     margin-top: 10px;
@@ -18,19 +20,37 @@ const StatGroup = styled.span`
     }
 `;
 
-const RepoStats = () => {
-    return (
-        <RepoStatsWrapper>
-            <StatGroup>
-                <i className="fas fa-star"></i>
-                <span>1</span>
-            </StatGroup>
-            <StatGroup>
-                <i className="fas fa-code-branch"></i>
-                <span>1</span>
-            </StatGroup>
-        </RepoStatsWrapper>
-    );
-};
+class RepoStats extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { forksCount: 0, stargazersCount: 0 };
+    }
+
+    async componentDidMount() {
+        const {
+            data: { forks_count, stargazers_count },
+        } = await axios({
+            method: 'GET',
+            url: 'https://api.github.com/repos/abhijithvijayan/abhijithvijayan.in',
+        });
+        this.setState({ forksCount: forks_count, stargazersCount: stargazers_count });
+    }
+
+    render() {
+        const { forksCount, stargazersCount } = this.state;
+        return (
+            <RepoStatsWrapper>
+                <StatGroup>
+                    <i className="fas fa-star"></i>
+                    <span>{stargazersCount}</span>
+                </StatGroup>
+                <StatGroup>
+                    <i className="fas fa-code-branch"></i>
+                    <span>{forksCount}</span>
+                </StatGroup>
+            </RepoStatsWrapper>
+        );
+    }
+}
 
 export default RepoStats;
