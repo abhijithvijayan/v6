af24a1d92d1f3febc19088169602b35001543b05
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-08-08 21:38:08 +0530

    set navbar to hide/show on scroll

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Navbar } from 'react-bootstrap';
 
+import { throttle } from '../../utils';
 import HamburgerIcon from './HamburgerIcon';
 import VerticalHeader from './VerticalHeader';
 import HorizontalHeader from './HorizontalHeader';
@@ -14,8 +15,17 @@ const StyledHeader = styled.header`
     top: 0px;
     transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0s;
     width: 100%;
-    height: 100px;
-    transform: translateY(0px);
+    height: ${props => {
+        return props.scrollDirection === 'none' ? props.theme.navHeight : props.theme.navScrollHeight;
+    }};
+    box-shadow: ${props => {
+        return props.scrollDirection === 'up' ? `0px 2px 4px ${props.theme.shadow}` : 'none';
+    }};
+    transform: translateY(
+        ${props => {
+            return props.scrollDirection === 'down' ? `-${props.theme.navScrollHeight}` : '0px';
+        }}
+    );
     padding: 0px 50px;
     background-color: ${props => {
         return props.theme.purple;
@@ -37,21 +47,76 @@ const NavbarWrapper = styled(Navbar)`
     justify-content: flex-start;
 `;
 
+const DELTA = 5;
+
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = { isSidebarOpen: false };
+
+        this.state = {
+            isMounted: false,
+            scrollDirection: 'none',
+            lastScrollTop: 0,
+            isSidebarOpen: false,
+        };
     }
+
+    componentDidMount() {
+        setTimeout(() => {
+            return this.setState({ isMounted: true });
+        }, 100);
+
+        // if (typeof window !== 'undefined') {}
+        window.addEventListener('scroll', () => {
+            return throttle(this.handleScroll());
+        });
+    }
+
+    componentWillUnmount() {
+        this.setState({ isMounted: false });
+
+        // if (typeof window !== 'undefined') {}
+        window.removeEventListener('scroll', () => {
+            return this.handleScroll();
+        });
+    }
+
+    handleScroll = () => {
+        const { isMounted, scrollDirection, lastScrollTop } = this.state;
+        const fromTop = window.scrollY;
+
+        // Make sure they scroll more than DELTA
+        if (!isMounted || Math.abs(lastScrollTop - fromTop) <= DELTA) {
+            return;
+        }
+
+        if (fromTop < DELTA) {
+            this.setState({ scrollDirection: 'none' });
+        } else if (fromTop > lastScrollTop && fromTop > 100) {
+            if (scrollDirection !== 'down') {
+                this.setState({ scrollDirection: 'down' });
+            }
+        } else if (fromTop + window.innerHeight < document.body.scrollHeight) {
+            if (scrollDirection !== 'up') {
+                this.setState({ scrollDirection: 'up' });
+            }
+        }
+
+        this.setState({ lastScrollTop: fromTop });
+    };
 
     toggleSidebar = toggle => {
         this.setState({ isSidebarOpen: toggle });
     };
 
     render() {
-        const { isSidebarOpen } = this.state;
+        const { isSidebarOpen, scrollDirection } = this.state;
 
         return (
-            <StyledHeader className="d-flex justify-content-end pr-0 pr-sm-3 pr-md-4 pr-lg-5">
+            <StyledHeader
+                className="d-flex justify-content-end pr-0 pr-sm-3 pr-md-4 pr-lg-5"
+                scrollDirection={scrollDirection}
+            >
                 <NavbarWrapper collapseOnSelect expand="sm">
                     <HamburgerIcon toggleSidebar={this.toggleSidebar} isSidebarOpen={isSidebarOpen} />
                     <HorizontalHeader />
