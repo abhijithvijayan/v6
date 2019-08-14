e076e54ff32e6bfe6b25b2e098509444b55b65d7
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-08-14 15:25:36 +0530

    add config file to store local meta data infos

---
+++ config/index.js
@@ -0,0 +1,69 @@
+module.exports = {
+    name: 'Abhijith Vijayan',
+
+    siteTitle: 'Abhijith Vijayan | Front-End Software Engineer',
+    siteDescription: "I'm a Computer Enthusiast with experience in various software branches.",
+    siteKeywords:
+        'Abhijith-Vijayan, Abhijith-V, abhijith-v, abhijithvijayan, abhijith-vijayan, abhijithv, _abhijithvijayan, _abhijithv, Abhijith-V-CEA, Abhijith-Vijayan-CEA, Abhijith-V-pathanamthitta, Abhijith-Vijayan-pathanamthitta, Abhijith-V-IHRD, Abhijith-Vijayan-IHRD, Abhijith-Vijayan-theBlueDoor, Abhijith-Vijayan-dev, abhijith-vijayan-developer, Abhijith-V-rose-dale-senior-secondary-school-chandanappally, web-developer-in-pathanamthitta, abhijithvijayan-ceadoor, abhijith-vijayan-web-developer',
+    siteUrl: 'https://abhijithvijayan.in',
+    siteLanguage: 'en_US',
+
+    GRID_LIMIT: 6,
+
+    email: 'iam@abhijithvijayan.in',
+    repoAPIendpoint: 'https://api.github.com/repos/abhijithvijayan/abhijithvijayan.in',
+
+    socialMedia: [
+        {
+            name: 'Github',
+            icon: 'fab fa-github',
+            url: 'https://github.com/abhijithvijayan/',
+        },
+        {
+            name: 'LinkedIn',
+            icon: 'fab fa-linkedin-in',
+            url: 'https://in.linkedin.com/in/abhijith-vijayan/',
+        },
+        {
+            name: 'Stack Overflow',
+            icon: 'fab fa-stack-overflow',
+            url: 'https://stackoverflow.com/users/9387542/abhijithvijayan?tab=profile',
+        },
+        {
+            name: 'Behance',
+            icon: 'fab fa-behance-square',
+            url: 'https://www.behance.net/abhijithvijayan',
+        },
+        {
+            name: 'Medium',
+            icon: 'fab fa-medium-m',
+            url: 'https://medium.com/@abhijithvijayan',
+        },
+        {
+            name: 'Twitter',
+            icon: 'fab fa-twitter',
+            url: 'https://twitter.com/_abhijithv',
+        },
+    ],
+
+    navLinks: [
+        {
+            name: 'About',
+            url: '#about',
+        },
+        {
+            name: 'Experience',
+            url: '#experience',
+        },
+        {
+            name: 'Work',
+            url: '#projects',
+        },
+        {
+            name: 'Contact',
+            url: '#contact',
+        },
+    ],
+
+    navyColor: '#0a192f',
+};
