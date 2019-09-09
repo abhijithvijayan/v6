0f9de40ab37e00f6d1c751cf8e4f6eb0759dad77
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-09-09 20:10:42 +0530

    add animation to highlights section

--- components/Projects/ProjectItemWrapper.js
+++ components/Projects/ProjectItemWrapper.js
@@ -11,7 +11,7 @@ const Wrapper = styled(Row)`
 `;
 
 const ProjectItemWrapper = ({ children }) => {
-    return <Wrapper>{children}</Wrapper>;
+    return <Wrapper className="wow fadeInUp">{children}</Wrapper>;
 };
 
 export default ProjectItemWrapper;
--- components/Projects/ProjectsHeader.js
+++ components/Projects/ProjectsHeader.js
@@ -4,7 +4,7 @@ import { Col, Row } from 'react-bootstrap';
 const ProjectsHeader = () => {
     return (
         <Row>
-            <Col md={12}>
+            <Col md={12} className="wow fadeInUp">
                 <h2 className="d-flex align-items-center line__title">Some Things I've Built</h2>
             </Col>
         </Row>
