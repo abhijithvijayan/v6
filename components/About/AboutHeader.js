e2e40c1905184b16b776df5a7af58db7f6c41a6b
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-09-09 20:06:34 +0530

    add animation effect to about & experience section

--- components/About/AboutHeader.js
+++ components/About/AboutHeader.js
@@ -4,7 +4,7 @@ import { Col, Row } from 'react-bootstrap';
 const AboutHeader = ({ title }) => {
     return (
         <Row>
-            <Col md={12}>
+            <Col md={12} className="wow fadeInUp">
                 <h2 className="d-flex align-items-center line__title">{title}</h2>
             </Col>
         </Row>
--- components/Experience/ExperienceHeader.js
+++ components/Experience/ExperienceHeader.js
@@ -4,7 +4,7 @@ import { Row, Col } from 'react-bootstrap';
 const ExperienceHeader = () => {
     return (
         <Row>
-            <Col md={12}>
+            <Col md={12} className="wow fadeInUp">
                 <h2 className="d-flex align-items-center line__title">What I'm doing.</h2>
             </Col>
         </Row>
