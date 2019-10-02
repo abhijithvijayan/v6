f9e23445f40260edb0accb9c79fe52dd69fbcbf6
Yannick <yannick1691@users.noreply.github.com>
2019-10-02 11:51:37 +0200

    Feature/pwa support (#14)
    * Adds PWA support to the application
    
    * Updated config to use the withPlugins array

--- next.config.js
+++ next.config.js
@@ -5,8 +5,9 @@ const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withFonts = require('next-fonts');
 const withPlugins = require('next-compose-plugins');
+const withOffline = require('next-offline');
 
-module.exports = withPlugins([withCSS, withSass, withImages, withFonts], {
+module.exports = withPlugins([withCSS, withSass, withImages, withFonts, withOffline], {
     webpack(config, options) {
         config.module.rules.push({
             test: /\.md$/,
