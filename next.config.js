e880289dcdb44f24633b1fba5d54b48c096ac086
Yannick <yannick.frisart@outlook.com>
2019-10-02 11:40:32 +0200

    Adds PWA support to the application

--- next.config.js
+++ next.config.js
@@ -5,19 +5,22 @@ const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withFonts = require('next-fonts');
 const withPlugins = require('next-compose-plugins');
+const withOffline = require('next-offline');
 
-module.exports = withPlugins([withCSS, withSass, withImages, withFonts], {
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.md$/,
-            use: 'raw-loader',
-        });
-        return config;
-    },
+module.exports = withOffline(
+    withPlugins([withCSS, withSass, withImages, withFonts], {
+        webpack(config, options) {
+            config.module.rules.push({
+                test: /\.md$/,
+                use: 'raw-loader',
+            });
+            return config;
+        },
 
-    exportPathMap() {
-        return {
-            '/': { page: '/' },
-        };
-    },
-});
+        exportPathMap() {
+            return {
+                '/': { page: '/' },
+            };
+        },
+    })
+);
