33e7208eaf6aeb78701cf193eb2e99ff8b43b153
Muhammad Ramadhan Rahmat <muhamadramadhanrahmat@gmail.com>
2019-10-20 14:44:18 +0700

    Ftr/add animation delay to project cards on expansion (#25)
    * Override wow js animation and adding initial opacity
    
    * Set `animation-time` to 1s to match wow js default time

--- components/OtherProjects/ProjectCard.js
+++ components/OtherProjects/ProjectCard.js
@@ -7,6 +7,9 @@ import HeaderLinksHolder from './HeaderLinksHolder';
 import CardSubtitleHolder from './CardSubtitleHolder';
 
 const CardWrapper = styled.div`
+    opacity: 0;
+    animation-fill-mode: forwards;
+    animation-duration: 1s;
     display: flex;
     justify-content: space-between;
     flex-direction: column;
