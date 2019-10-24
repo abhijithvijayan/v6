b08611c2e9986b183ced9457deab2ee36b976ec9
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-24 08:44:48 +0530

    Fixes #29 use styled components props for setting vertical movement

--- components/Experience/Experience.js
+++ components/Experience/Experience.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import TitleItem from './TitleItem';
@@ -14,7 +14,40 @@ const ExperienceWrapper = styled.div`
     margin: 0px auto;
 `;
 
+const Highlighter = styled.span`
+    display: block;
+    width: 2px;
+    height: 42px;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    z-index: 10;
+    background: rgb(100, 255, 218);
+    border-radius: 2px;
+    transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1) 0.1s;
+    transform: translateY(
+        ${props => {
+            return props.activeTabId > 0 ? props.activeTabId * 41 : 0;
+        }}px
+    );
+
+    @media screen and (max-width: 575px) {
+        width: 100%;
+        height: 2px;
+        top: auto;
+        bottom: 0px;
+        max-width: 152px;
+        transform: translateX(
+            ${props => {
+                return props.activeTabId > 0 ? props.activeTabId * 152 : 0;
+            }}px
+        );
+    }
+`;
+
 const Experience = ({ content }) => {
+    const [activeTabId, setActiveTabId] = useState(0);
+
     if (!content.length) {
         return null;
     }
@@ -28,8 +61,11 @@ const Experience = ({ content }) => {
                             const {
                                 attributes: { title },
                             } = item;
-                            return <TitleItem title={title} id={index + 1} key={index} />;
+                            return (
+                                <TitleItem setActiveTabId={setActiveTabId} title={title} id={index + 1} key={index} />
+                            );
                         })}
+                        <Highlighter activeTabId={activeTabId} />
                     </TitlesHolder>
                     <DetailsHolder>
                         {content.map((item, index) => {
