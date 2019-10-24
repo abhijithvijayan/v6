4133ded1a4a96d959b3c1aa52d915042ec22ac04
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-24 08:45:16 +0530

    set activeTabId with hooks on navtabs click

--- components/Experience/TitleItem.js
+++ components/Experience/TitleItem.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 
-const TitleItem = ({ title, id }) => {
+const TitleItem = ({ title, id, setActiveTabId }) => {
     const handleClick = e => {
         const { target } = e;
-        const highLighter = document.getElementById('active__highlighter');
         if (!target.classList.contains('active')) {
             const trimmedId = target.id.slice(8);
-            if (window.outerWidth >= 583) {
-                highLighter.style = `transform: translateY(${41 * (trimmedId - 1)}px)`;
-            } else {
-                // Mobile context
-
-                const prevTab = document.getElementById(`exp-tab-${trimmedId - 1}`);
-
-                highLighter.setAttribute(
-                    'style',
-                    `transform: translateX(${target.offsetLeft}px); width: ${target.offsetWidth}px;`
-                );
-            }
+            setActiveTabId(trimmedId - 1);
         }
     };
 
